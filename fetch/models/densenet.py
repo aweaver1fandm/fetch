7d4ad641593e395549b1964e355c9b595f9c0a94
Tony Weaver <aweaver1@fandm.edu>
2024-09-18 13:43:43 -0400

    Generated images of all the model structures

--- fetch/models/densenet.py
+++ fetch/models/densenet.py
@@ -7,13 +7,3 @@ import torch.nn as nn
 
 # Use GPU if available
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-# Set the different model architecture parameters
-model_parameters={}
-model_parameters['densenet121'] = [6,12,24,16]
-model_parameters['densenet169'] = [6,12,32,32]
-model_parameters['densenet201'] = [6,12,48,32]
-
-# growth rate
-k = 32
-compression_factor = 0.5
