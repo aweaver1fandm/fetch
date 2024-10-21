a10d8696bf1c1d39212f96e85800d3d5ed6f11b5
Tony Weaver <aweaver1@fandm.edu>
2024-10-21 15:48:54 -0400

    Changes to load model without classifier layer

--- fetch/train.py
+++ fetch/train.py
@@ -245,11 +245,17 @@ def main():
         # will load even though the outfeatures will be k now
         freq_model_path = f"model_weights/{args.freq_model}_freq.pth"
         freq_model = TorchvisionModel(args.freq_model, out_features=k)
-        freq_model.load_state_dict(torch.load(freq_model_path, weights_only=True), strict=False)
+        state_dict = torch.load(freq_model_path)
+        new_state_dict = {k: v for k, v in state_dict.items() if not k.startswith("classifier")}
+        freq_model.load_state_dict(new_state_dict, strict=False)
+        #freq_model.load_state_dict(torch.load(freq_model_path, weights_only=True), strict=False)
 
         dm_model_path = f"model_weights/{args.dm_model}_dm.pth"
         dm_model = TorchvisionModel(args.dm_model, out_features=k)
-        dm_model.load_state_dict(torch.load(freq_model_path, weights_only=True), strict=False)
+        state_dict = torch.load(dm_model_path)
+        new_state_dict = {k: v for k, v in state_dict.items() if not k.startswith("classifier")}
+        dm_model.load_state_dict(new_state_dict, strict=False)
+        #dm_model.load_state_dict(torch.load(freq_model_path, weights_only=True), strict=False)
 
         # Setup model
         model = TorchvisionModel(args.freq_model, args.dm_model, out_features=k).to(DEVICE)
