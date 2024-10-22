c81cdc184e5364b2e6f97aaaf2a22b5689e9dace
Tony Weaver <aweaver1@fandm.edu>
2024-10-22 09:54:45 -0400

    Fixed incorrect function argument variable

--- fetch/train.py
+++ fetch/train.py
@@ -249,7 +249,7 @@ def main():
             train_loop(tr_dataloader, model, loss_fn, optimizer, args.batch_size)
 
             # Validate the model and track best model perfomance
-            avg_vloss = validate_loop(v_dataloader, model, "all", loss_fn, args.prob)
+            avg_vloss = validate_loop(v_dataloader, model, "all", loss_fn, args.probability)
             if avg_vloss < best_vloss:
                 best_vloss = avg_vloss
                 best_k = k
