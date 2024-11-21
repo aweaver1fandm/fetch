d57b321b924c66d2fc0071a7db7c61ea7534fc99
Tony Weaver <aweaver1@fandm.edu>
2024-11-21 12:58:33 -0500

    Modified pulsar data to clean up data on initial load
    Data clean up was taking place on each get_item really slowing down processing

--- fetch/pulsar_data.py
+++ fetch/pulsar_data.py
@@ -86,6 +86,7 @@ class PulsarData(Dataset):
         return self.num_observations
 
     def __getitem__(self, index: int)-> tuple:
+        '''
         ft_data = np.empty((*self.ft_dim, self.n_channels))
         dt_data = np.empty((*self.dt_dim, self.n_channels))
 
@@ -102,7 +103,9 @@ class PulsarData(Dataset):
         dt_data = np.reshape(dt_data, (self.n_channels, *self.dt_dim))
 
         # Return data as PyTorch Tensor
-        return torch.from_numpy(ft_data), torch.from_numpy(dt_data), torch.tensor(self.labels[index])
+        return torch.from_numpy(ft_data), torch.from_numpy(dt_data), torch.tensor(self.labels[index])'''
+
+        return torch.from_numpy(self.ft_data[index]), torch.from_numpy(self.dt_data[index]), torch.tensor(self.labels[index])
         
     def _data_from_h5(self, file: str) -> None:
         r""" Reads a single .h5 file 
@@ -164,9 +167,23 @@ class PulsarData(Dataset):
             print(f"ERROR: Data shape {data_dims} does not match expected dimensions {self.ft_dim}", flush=True)
             sys.exit(1)
 
-        self.ft_data = np.append(self.ft_data, freq_data, axis=0)
-        self.dt_data = np.append(self.dt_data, dm_data, axis=0)
         self.num_observations += num_observations
+
+        # Clean up the data bit
+        for idx in range(num_observations):
+            ft_data = s.detrend(np.nan_to_num(np.array(freq_data[idx], dtype=np.float32).T))
+            ft_data /= np.std(ft_data)
+            ft_data -= np.median(ft_data)
+        
+            dt_data = np.nan_to_num(np.array(dm_data[idx], dtype=np.float32))
+            dt_data /= np.std(dt_data)
+            dt_data -= np.median(dt_data)
+
+            ft_data = np.reshape(ft_data, (self.n_channels, *self.ft_dim))
+            dt_data = np.reshape(dt_data, (self.n_channels, *self.dt_dim))
+
+            self.ft_data = np.append(self.ft_data, ft_data, axis=0)
+            self.dt_data = np.append(self.dt_data, dt_data, axis=0)
         
         # Handle the labels if they exist
         if "data_labels" in data:
