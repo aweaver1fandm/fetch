1dd0ac13772347414c626c1df0bdba6a663e1de2
Tony Weaver <aweaver1@fandm.edu>
2024-12-06 13:50:18 -0500

    Removed old call to constants

--- fetch/predict.py
+++ fetch/predict.py
@@ -12,7 +12,7 @@ import torch
 from torch.utils.data import DataLoader
 
 from fetch.pulsar_data import PulsarData
-from fetch.model import PulsarModel, MODELPARAMS
+from fetch.model import PulsarModel
 
 # Use GPU if available
 DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
